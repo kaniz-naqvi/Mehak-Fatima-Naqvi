e7fad2c993c24b4d9d6195bd13f60ef028405577
Mehak <mehak313naqvi@gmail.com>
2025-01-20 22:07:00 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <button class="btn btn-primary">Click Me</button>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/theme.js
@@ -0,0 +1,11 @@
+export const lightTheme = {
+  background: "#fff",
+  text: "#000",
+  accent: "#d10580",
+};
+
+export const darkTheme = {
+  background: "#000",
+  text: "#fff",
+  accent: "#d10580",
+};
